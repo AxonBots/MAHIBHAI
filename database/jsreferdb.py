fa2a7979e21910b4500b6412f16b68e603d4ef9b
AxonBots <148529190+AxonBots@users.noreply.github.com>
2025-04-01 00:25:55 +0530

    Update jsreferdb.py

--- database/jsreferdb.py
+++ database/jsreferdb.py
@@ -1,39 +1,32 @@
-import pymongo
-from info import DATABASE_URI, DATABASE_NAME
-import logging
+from pymongo import MongoClient
+from info import DATABASE_URI_MAIN, DATABASE_NAME_MAIN
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
-
-myclient = pymongo.MongoClient(DATABASE_URI)
-mydb = myclient[DATABASE_NAME]
-
-
-class UserTracker:
+class ReferDB:
     def __init__(self):
-        self.user_collection = mydb["referusers"]
-        self.refer_collection = mydb["refers"]
+        self.client = MongoClient(DATABASE_URI_MAIN)
+        self.db = self.client[DATABASE_NAME_MAIN]
+        self.user_col = self.db["referusers"]
+        self.ref_col = self.db["refers"]
 
     def add_user(self, user_id):
-        if not self.is_user_in_list(user_id):
-            self.user_collection.insert_one({'user_id': user_id})
+        if not self.user_col.find_one({"user_id": user_id}):
+            self.user_col.insert_one({"user_id": user_id})
 
     def remove_user(self, user_id):
-        self.user_collection.delete_one({'user_id': user_id})
+        self.user_col.delete_one({"user_id": user_id})
 
     def is_user_in_list(self, user_id):
-        return bool(self.user_collection.find_one({'user_id': user_id}))
+        return bool(self.user_col.find_one({"user_id": user_id}))
 
-    def add_refer_points(self, user_id: int, points: int):
-        self.refer_collection.update_one(
-            {'user_id': user_id},
-            {'$set': {'points': points}},
+    def add_refer_points(self, user_id, points):
+        self.ref_col.update_one(
+            {"user_id": user_id},
+            {"$set": {"points": points}},
             upsert=True
         )
 
-    def get_refer_points(self, user_id: int):
-        user = self.refer_collection.find_one({'user_id': user_id})
-        return user.get('points') if user else 0
+    def get_refer_points(self, user_id):
+        user = self.ref_col.find_one({"user_id": user_id})
+        return user.get("points", 0) if user else 0
 
-
-referdb = UserTracker()
+refer_db = ReferDB()
